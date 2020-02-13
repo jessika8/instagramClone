c8ca4712f0bdf1b888128dc4f708334444de72f7
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-13 16:52:39 +0000

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header.js'
-
+import Footer from './components/Footer.js'
+// import Profile from './images/bevs.png'
 export default class App extends Component {
+
+  
+
+
+
+
   render() {
     return (
       <div>
         <Header instagram="Instagram" />
+
+
+
+
+
+
+
+
+
+
+        <Footer footer="Footer" />
       </div>
     )
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Profile from '../images/bevs.png'
+
+export default function Footer() {
+    return (
+        <footer className = "footer1">
+        <ion-icon name="home-outline"></ion-icon>
+        <ion-icon name="search-outline"></ion-icon>
+        <ion-icon name="add-circle-outline"></ion-icon>
+        <ion-icon name="heart-outline"></ion-icon>
+        <ion-icon name="person-outline"></ion-icon>
+        {/* <img src= {Profile} alt= "Bevs" />  */}
+        </footer>
+    )
+}
