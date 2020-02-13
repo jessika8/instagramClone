9deb7aae7f8fd6d4c702fcf51a8e1857ba940ff1
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-13 13:43:35 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default function Header(props) {
+    return (
+        <div className="mainHeader">
+            <ion-icon className="headerIcon" name="camera-outline"></ion-icon>
+            <h3>{props.instagram}</h3>
+            <ion-icon className="headerIcon" name="paper-plane-outline"></ion-icon>
+        </div>
+    )
+}
